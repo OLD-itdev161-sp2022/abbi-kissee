6d6a4d55b91d3d19c59b76df8347774ccd7709b0
Abbi Kissee <abbikissee@Abbis-MBP.attlocal.net>
2022-02-16 15:18:34 -0600

    Activity 4

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import { render } from 'express/lib/response';
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          GoodThings
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
