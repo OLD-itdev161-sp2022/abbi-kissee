2d945b4c32d9115ad6c775ef6aee64e8292dedd8
Abbi Kissee <abbikissee@Abbis-MBP.attlocal.net>
2022-02-16 15:46:07 -0600

    Completed Activity 4

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,31 @@
-import { render } from 'express/lib/response';
 import React from 'react';
 import './App.css';
+import axios from 'axios';
 
 class App extends React.Component {
+  state = {
+    data: null
+  }
+
+  componentDidMount() {
+    axios.get('http://localhose:5000')
+      .then((response) => {
+        this.setState({
+          data: response.data
+        })
+      })
+      .catch((error) => {
+        console.error(`Error fetching data ${error}`);
+      })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           GoodThings
         </header>
+        {this.state.data}
       </div>
     );
   }
